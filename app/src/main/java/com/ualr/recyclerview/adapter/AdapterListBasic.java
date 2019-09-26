d5bd951ad0f1439f095ba1b988f385ab6ab68f5c
irconde <ivanrodriguezconde@gmail.com>
2019-09-25 21:47:41 -0500

    Handling click on recyclerview items

--- app/src/main/java/com/ualr/recyclerview/adapter/AdapterListBasic.java
+++ app/src/main/java/com/ualr/recyclerview/adapter/AdapterListBasic.java
@@ -20,70 +20,73 @@ import java.util.List;
  * Created by irconde on 2019-09-25.
  */
 
-// TODO 11: The new Adapter class must be derived from RecyclerView.Adapter.
-// We will have to implement the onCreateViewHolder, onBindViewHolder, and getItemCount methods from the RecyclerView.Adapter class
+// TODO 01: We have to define a new base class that represents any item in the list: both section headers and people rows
+// TODO 03: Create a new class that extends from Item class and represents a section header
+// TODO 05: We have to replace any reference to People class with a reference to Item class
 
 public class AdapterListBasic extends RecyclerView.Adapter{
 
-    // TODO 12: Define a variable that will contain the data source that is passed into the constructor
+    // TODO 09: We'll define two constant values that represent the two types of items that we have in our recyclerView
+
     private List<People> mItems;
     private Context mContext;
 
+    private OnItemClickListener mOnItemClickListener;
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, People obj, int position);
+    }
+
+    public void setOnItemClickListener(final OnItemClickListener mItemClickListener) {
+        this.mOnItemClickListener = mItemClickListener;
+    }
+
     public AdapterListBasic(Context context, List<People> items) {
-        // TODO 13: The constructor copies the people collection to the corresponding variable
         this.mItems = items;
         this.mContext = context;
     }
 
-    // TODO 14: Add the onCreateViewHolder method
-    // This method is called by the layout manager when the RecyclerView needs a new view holder to represent an item
-    // So,it inflates the item view from the view's layout file and wraps the view in a new ViewHolder instance
-    // TODO 15: We need to define the layout used for each item in the RecyclerView
-    // TODO 16: We also need to define the ViewHolder class, used to hold a reference to the different widgets within each row item
+    // TODO 08: Somehow, we need to know if an item in the list is a person or just the header of a section.
+    //  We must implement the method getItemViewType, that returns the view type of a item at position
+
+    // TODO 09: We'll define two constant values that represent the two types of items that we have in our recyclerView
+
+    // TODO 10: Modify onCreateViewHolder. We have to create the proper view holder based on the type of item
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        // TODO 17: We implement the onCreateViewHolder method
-        RecyclerView.ViewHolder vh;
-        // TODO 18: Inflate the view for a row item
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_people_chat, parent, false);
-        // TODO 19: Create a ViewHolder to hold view references inside the view
+        RecyclerView.ViewHolder vh = null;
+        View itemView = null;
+
+        itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_people_chat, parent, false);
         vh = new PersonViewHolder(itemView);
-        // The resulting view holder instance is returned back to the called (the layout manager)
+
         return vh;
     }
 
-    // TODO 20: Add the onBindViewHolder method
-    // This method is called when the layout manager is ready to display a particular view in the Recycler's view
-    // visible screen area
-    // Basically, the method fills the item at the specified row position with content from the data source
-    // Gets the person information for the specified row position (and the corresponding image) and copies this data to the associated views.
-    // Views are located via references stored in the view holder object (passed in through the holder parameter)
+    // TODO 11: Modify onBindViewHolder. We have populate each item type with the corresponding data
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        PersonViewHolder viewHolder = (PersonViewHolder)holder;
-        // Load information regarding a specific person from people collection
-        People p = mItems.get(position);
-        // Populate the corresponding TextView
-        viewHolder.name.setText(p.name);
-        // Populate the corresponding ImageView.
-        // TODO 21: We can use the glide library just to make the scrolling faster and smoother
-        //viewHolder.image.setImageResource(p.image);
-        Tools.displayImageRound(mContext, viewHolder.image, p.image);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
+        People person = mItems.get(position);
+        PersonViewHolder viewHolder = (PersonViewHolder) holder;
+        viewHolder.name.setText(person.getName());
+        Tools.displayImageRound(mContext, viewHolder.image, person.getImage());
+        viewHolder.lyt_parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mOnItemClickListener.onItemClick(view, (People) mItems.get(position), position);
+            }
+        });
     }
 
-    // TODO 22: We add the getItemCount method
-    // It returns the number of items in the data collection
     @Override
     public int getItemCount() {
         return this.mItems.size();
     }
 
-    // TODO 16: Typically, we'll define the ViewHolder class as an inner class
-    // Each instance holds references to the ImageView and TextView of an associated row item
     public class PersonViewHolder extends RecyclerView.ViewHolder {
         public ImageView image;
         public TextView name;
@@ -96,11 +99,15 @@ public class AdapterListBasic extends RecyclerView.Adapter{
          *
          * @param v parent item view reference
          */
-        public PersonViewHolder(View v) {
+        public PersonViewHolder(@NonNull View v) {
             super(v);
             image = v.findViewById(R.id.image);
             name = v.findViewById(R.id.name);
             lyt_parent = v.findViewById(R.id.lyt_parent);
         }
     }
+
+    // TODO 06: We have to define a specific ViewHolder for the section header views
+    // TODO 07: We have to define a specific layout for the section header views
+
 }
--- app/src/main/java/com/ualr/recyclerview/model/People.java
+++ app/src/main/java/com/ualr/recyclerview/model/People.java
@@ -5,18 +5,49 @@ import android.graphics.drawable.Drawable;
 /**
  * Created by irconde on 2019-09-25.
  */
+// TODO 02: Modify People class. Extend from Item abstract class and set section value and set section value
 public class People {
-    public int image;
-    public Drawable imageDrw;
-    public String name;
-    public String email;
-    public boolean section = false;
+    private int image;
+    private String name;
+    private Drawable imageDrw;
+    private String email;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public Drawable getImageDrw() {
+        return imageDrw;
+    }
+
+    public void setImageDrw(Drawable imageDrw) {
+        this.imageDrw = imageDrw;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public People() {
     }
 
-    public People(String name, boolean section) {
+    public People(String name) {
         this.name = name;
-        this.section = section;
     }
 }
